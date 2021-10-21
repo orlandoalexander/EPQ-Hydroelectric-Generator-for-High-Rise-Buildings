b2d1e557304a911e87afa32892eab06db1089f43
orlandoalexander <67097862+orlandoalexander@users.noreply.github.com>
2021-10-21 15:36:05 +0100

    Update and rename test_pi.py to barrier.py

--- barrier.py
+++ barrier.py
@@ -7,7 +7,16 @@ GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(26, GPIO.OUT)
 GPIO.setup(21, GPIO.OUT)
+
 time.sleep(3)
+
+GPIO.output(21, GPIO.HIGH)
+
+time.sleep(1)
+
+GPIO.output(21, GPIO.LOW)
+
+time.sleep(10)
 
 GPIO.output(26, GPIO.HIGH)
 GPIO.output(21, GPIO.HIGH)
@@ -17,10 +26,4 @@ time.sleep(1)
 GPIO.output(26, GPIO.LOW)
 GPIO.output(21, GPIO.LOW)
 
-time.sleep(3)
 
-GPIO.output(21, GPIO.HIGH)
-
-time.sleep(1)
-
-GPIO.output(21, GPIO.LOW)
