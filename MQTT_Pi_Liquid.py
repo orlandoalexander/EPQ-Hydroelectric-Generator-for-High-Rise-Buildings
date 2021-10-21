6bd607a5667d74ed94447ec94c4aec8832f0db15
orlandoalexander <67097862+orlandoalexander@users.noreply.github.com>
2021-10-21 16:03:03 +0100

    Update MQTT_Pi_Liquid.py

--- MQTT_Pi_Liquid.py
+++ MQTT_Pi_Liquid.py
@@ -1,22 +1,21 @@
-import paho.mqtt.client as mqtt
-import RPi.GPIO as GPIO
-import time
+# import paho.mqtt.client as mqtt
+# import RPi.GPIO as GPIO
+# import time
 
 
-def on_connect(client, userdata, flags, rc):
-    if rc == 0: # if connection is successful
-        print("Connected")
-    else:
-        print("not connected")
-        # attempts to reconnect
-        client.on_connect = on_connect
-        client.username_pw_set(username="yrczhohs", password="qPSwbxPDQHEI")
-        client.connect("hairdresser.cloudmqtt.com", 18973)
+# def on_connect(client, userdata, flags, rc):
+#     if rc == 0: # if connection is successful
+#         print("Connected")
+#     else:
+#         # attempts to reconnect
+#         client.on_connect = on_connect
+#         client.username_pw_set(username="yrczhohs", password="qPSwbxPDQHEI")
+#         client.connect("hairdresser.cloudmqtt.com", 18973)
 
-client = mqtt.Client()
-client.username_pw_set(username="yrczhohs", password="qPSwbxPDQHEI")
-client.on_connect = on_connect # creates callback for successful connection with broker
-client.connect("hairdresser.cloudmqtt.com", 18973) # parameters for broker web address and port number
+# client = mqtt.Client()
+# client.username_pw_set(username="yrczhohs", password="qPSwbxPDQHEI")
+# client.on_connect = on_connect # creates callback for successful connection with broker
+# client.connect("hairdresser.cloudmqtt.com", 18973) # parameters for broker web address and port number
  
 
 # Use BCM GPIO references
